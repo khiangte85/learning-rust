7dc71e58d68928321cfef15dc6ea4bc2314bb414
awmtea <awmtea105.infinity@gmail.com>
2023-12-08 16:56:31 +0700

    traits

---
+++ traits/src/main.rs
@@ -0,0 +1,59 @@
+pub trait Summary {
+    fn author(&self) -> String;
+
+    fn summarize(&self) -> String;
+}
+
+pub struct Article {
+    author: String,
+    title: String,
+    content: String,
+}
+
+pub struct Tweet {
+    username: String,
+    tweet: String,
+}
+
+impl Summary for Article {
+    fn author(&self) -> String {
+        format!("{}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{} - {}", self.title, self.content)
+    }
+}
+
+impl Summary for Tweet {
+    fn author(&self) -> String {
+        format!("{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}", self.tweet)
+    }
+}
+
+pub fn notify<T: Summary>(item: &T) {
+    println!("{} - {}", item.summarize(), item.author());
+}
+
+fn main() {
+    let article = Article {
+        author: String::from("Awmtea"),
+        title: String::from("Learning Rust"),
+        content: String::from("First day of learning Rust"),
+    };
+
+    let tweet = Tweet {
+        username: String::from("khiangte"),
+        tweet: String::from("My first tweet"),
+    };
+
+    println!("{}", article.summarize());
+    notify(&article);
+
+    println!("{}", tweet.summarize());
+    notify(&tweet);
+}
