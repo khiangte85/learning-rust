4b97a546e2c761604ffe8c0eede975984ce94960
awmtea <awmtea105.infinity@gmail.com>
2024-02-15 10:43:41 +0700

    improve minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -6,13 +6,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(s) => s,
+            None => return Err("Missing query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(s) => s,
+            None => return Err("Missing file path"),
+        };
 
         Ok(Config { query, file_path })
     }
@@ -29,20 +34,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
     println!(
         "Searching for text '{}' in \n\n{}\n\n and the results are \n",
         query, contents
     );
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|l| l.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
